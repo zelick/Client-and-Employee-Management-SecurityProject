60fa4c7052c036d6e9a18530abf68ffc8435a41f
anciii13 <anaa.radovanovic2001@gmail.com>
2024-05-11 15:18:04 +0200

    project setup

---
+++ back-end/SecurityProject/src/test/java/org/example/securityproject/SecurityProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.securityproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
