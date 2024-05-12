75b2e93c5c82fb9b97ad0ffe887618e81ac50555
anciii13 <anaa.radovanovic2001@gmail.com>
2024-05-12 12:51:11 +0200

    feat: registration form created

--- front-end/security-project/src/app/registration/registration.component.ts
+++ front-end/security-project/src/app/registration/registration.component.ts
@@ -1,6 +1,12 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { User } from '../model/user.model';
+import { UserRole } from '../model/userRole.model';
+import { ClientType } from '../model/clientType.model';
+import { ServicesPackage } from '../model/servicesPackage.model';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; // Dodajte ovu liniju
 
 @Component({
   selector: 'app-registration',
@@ -9,8 +15,32 @@ import { User } from '../model/user.model';
 })
 
 export class RegistrationComponent {
+  userRoles = Object.values(UserRole);
+  clientTypes = Object.values(ClientType);
+  servicesPackages = Object.values(ServicesPackage);
 
-  constructor(private router: Router) {
+  userData = {
+    email: '',
+    password: '',
+    name: '',
+    surname: '',
+    address: '',
+    city: '',
+    country: '',
+    phoneNumber: '',
+    role: UserRole.CLIENT,
+    clientType: ClientType.INDIVIDUAL,
+    servicesPackage: ServicesPackage.BASIC
+  };
+
+  confirmPassword: string = '';
+
+  get passwordMismatch() {
+    return this.userData.password !== this.confirmPassword;
   }
 
+  onSubmit() {
+    // Logika za slanje podataka na server ili dodatna obrada
+    console.log('USPESNO REGISTROVANJE: ' + this.userData.email);
+  }
 }
