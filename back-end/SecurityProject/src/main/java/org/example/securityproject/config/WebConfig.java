ddae419801da728a29410df273291426f9eb8e40
anciii13 <anaa.radovanovic2001@gmail.com>
2024-05-14 00:57:11 +0200

    feat: done setting up https on the front end

--- back-end/SecurityProject/src/main/java/org/example/securityproject/config/WebConfig.java
+++ back-end/SecurityProject/src/main/java/org/example/securityproject/config/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins("https://localhost:4200")
                         .allowedMethods(HttpMethod.GET.name(),
                                 HttpMethod.POST.name(),
                                 HttpMethod.DELETE.name(),
